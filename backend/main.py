bc0270d1b6605e69b706686eb7e046555ae23642
Petchjrwchim <159994998+Petchjrwchim@users.noreply.github.com>
2025-02-15 18:42:46 +0700

    update link page

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request, HTTPException
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 
-# Import the authentication router
-from api.routes import authentication
+#authentication function
+from api.routes.authentication import router as auth_router
 
 app = FastAPI()
 
@@ -22,9 +23,24 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="../Frontend/static"), name="static")
 templates = Jinja2Templates(directory="../Frontend/templates")
 
-# Include the authentication router
-app.include_router(authentication.router, prefix="/auth", tags=["Authentication"])
 
+app.include_router(auth_router)
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "message": "Welcome to Trade’n Offer"})
+async def index(request: Request):
+    session_token = request.cookies.get("session_token")
+    if not session_token:
+        return RedirectResponse(url="/login")
+    return templates.TemplateResponse("mainPage.html", 
+                                      {"request": request, "message": "Welcome to Trade’n Offer"})
+
+
+@app.get("/login")
+async def loginPage(request: Request):
+    return templates.TemplateResponse("authentication.html", 
+                                      {"request": request, "message": "Welcome to Trade’n Offer"})
+
+@app.get("/logout")
+async def logout():
+    response = RedirectResponse(url="login")
+    response.delete_cookie("session_token")
+    return response
