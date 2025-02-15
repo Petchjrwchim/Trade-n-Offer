89ebc909babd82028263c89d146675ba4bea695d
Petchjrwchim <159994998+Petchjrwchim@users.noreply.github.com>
2025-02-15 21:57:49 +0700

    reupload

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 
+from typing import List
 #authentication function
 from api.routes.authentication import router as auth_router
+from api.routes.item import router as item_router
 
 app = FastAPI()
 
@@ -23,17 +25,16 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="../Frontend/static"), name="static")
 templates = Jinja2Templates(directory="../Frontend/templates")
 
-
-
 app.include_router(auth_router)
+app.include_router(item_router)
+
 @app.get("/")
 async def index(request: Request):
     session_token = request.cookies.get("session_token")
     if not session_token:
         return RedirectResponse(url="/login")
     return templates.TemplateResponse("main_page.html", 
-                                      {"request": request, "message": "Welcome to Trade’n Offer"})
-
+                                      {"request": request})
 
 @app.get("/login")
 async def loginPage(request: Request):
@@ -45,4 +46,3 @@ async def logout():
     response = RedirectResponse(url="login")
     response.delete_cookie("session_token")
     return response
-
