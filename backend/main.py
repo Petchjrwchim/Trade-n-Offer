6561c4cc6cc1c78ad1df0f177ff812de9627c391
Parin15675 <149325918+Parin15675@users.noreply.github.com>
2025-02-17 19:34:35 +0700

    adad

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,21 @@
-from fastapi import FastAPI, Depends, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
-
-from typing import List
-#authentication function
 from api.routes.authentication import router as auth_router
 from api.routes.item import router as item_router
 
 app = FastAPI()
 
-# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins for development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Static and template mounting
 app.mount("/static", StaticFiles(directory="../Frontend/static"), name="static")
 templates = Jinja2Templates(directory="../Frontend/templates")
 
@@ -33,16 +27,19 @@ async def index(request: Request):
     session_token = request.cookies.get("session_token")
     if not session_token:
         return RedirectResponse(url="/login")
-    return templates.TemplateResponse("main_page.html", 
-                                      {"request": request})
+    return templates.TemplateResponse("main_page.html", {"request": request})
 
 @app.get("/login")
 async def loginPage(request: Request):
-    return templates.TemplateResponse("authentication.html", 
-                                      {"request": request, "message": "Welcome to Trade’n Offer"})
+    return templates.TemplateResponse("authentication.html", {"request": request, "message": "Welcome to Trade’n Offer"})
 
 @app.get("/logout")
 async def logout():
     response = RedirectResponse(url="login")
     response.delete_cookie("session_token")
     return response
+
+# Route ใหม่สำหรับ Trade’n Offer
+@app.get("/trade_offer")
+async def trade_offer_page(request: Request):
+    return templates.TemplateResponse("trade_offer.html", {"request": request})
