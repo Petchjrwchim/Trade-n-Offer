25699347ac81df7f42cd7a3ae29b9d966c6774e3
Petchjrwchim <petch.jrw.chim@gmail.com>
2025-03-31 21:17:18 +0700

    ง

--- backend/app/zodb_setup.py
+++ backend/app/zodb_setup.py
@@ -19,21 +19,9 @@ def commit_changes():
 
 def close_connection():
     try:
-        # Commit any pending transactions before closing
         commit_changes()
         connection.close()
     except Exception as e:
         print(f"Error closing ZODB connection: {str(e)}")
 
-
-def inspect_zodb():
-    # You can change 'trade_items' to the actual key you're using in your ZODB database
-    trade_items = root.get('trade_items', None)
-
-    if trade_items is None:
-        print("No data found in 'trade_items'")
-    else:
-        print("Trade Items in ZODB:")
-        for item_id, item_obj in trade_items.items():
-            print(f"ID: {item_id}, Name: , Price: , Category: ")
 
